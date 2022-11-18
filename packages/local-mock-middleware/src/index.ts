e9997965391b8740ff9dc7908d44457e0d739cc5
redstar08 <redstar08@qq.com>
2022-11-19 00:39:10 +0800

    feat: export generateTemplate to custom middleware

--- packages/local-mock-middleware/src/index.ts
+++ packages/local-mock-middleware/src/index.ts
@@ -1,7 +1,8 @@
+import { generateTemplate } from '@/core/constants'
 import expressMiddleware from '@/core/express-middleware'
 import koaMiddleware from '@/core/koa-middleware'
 
-export { koaMiddleware, expressMiddleware }
+export { generateTemplate, koaMiddleware, expressMiddleware }
 
 export const name = 'local-mock-middleware'
 
